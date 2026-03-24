5c36949b2559fa5bf855bef8a5e889d8ff9eab62
Julian Andres Agamez de Avila <zjulian1208@gmail.com>
2026-03-24 08:05:22 -0500

    Create S3_session_1.py

---
+++ S3_G2_JULIAN_AGAMEZ/S3_session_1.py
@@ -0,0 +1,9 @@
+# Día 1: Funciones
+## Crear una función - def nombre(): return/print ft Type Hints
+def coordenadas(x, y):
+    print(f"({x}, {y})")
+
+## Alcance de las variables - Funciones SIEMPRE deben ser cajas negras - Ingreso por parámetros, Salida por return.
+## Para revisión autónoma
+### Importancia de los type hints en tiempos de IA
+### Parámetros por defecto, *args, **kwargs
