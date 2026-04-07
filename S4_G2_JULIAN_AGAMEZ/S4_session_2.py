96fbdc5965705505919b6729fe0650c70dda4ecd
Julian Andres Agamez de Avila <zjulian1208@gmail.com>
2026-04-06 22:08:58 -0500

    Update S4_session_2.py

---
+++ S4_G2_JULIAN_AGAMEZ/S4_session_2.py
@@ -0,0 +1,16 @@
+#Día  2 Líbrerias, Modulos y Herencias
+## ¿Qué es un modulo? Un archivo .py que contine codigo reutilizable (funciones, clases, variables)
+## ¿Qué es una librería? Una colección de modulos agrupados
+## ¿Por qué modularizar? Para organizar el código, facilitar su mantenimiento y reutilización
+
+
+
+## 1. import completo: accedes con prefijo del modulo.funcion
+
+import math
+print(math.sqrt(144))
+print(math.pi)
+
+## 2. import especifico: accedes directamente a la función sin prefijo
+
+## 3
